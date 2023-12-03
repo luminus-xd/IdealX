cd35fa9e8130a121afbc8ef68bc45391a8bd8c5c
luminus-xd <eleline.dev@gmail.com>
2023-12-04 01:11:38 +0900

    GPT適用

---
+++ src/chatgpt.rs
@@ -0,0 +1,59 @@
+use reqwest;
+use reqwest::Error;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug)]
+struct ChatResponse {
+    choices: Vec<Choice>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Choice {
+    message: ResponseMessage,
+}
+
+#[derive(Deserialize, Debug)]
+struct ResponseMessage {
+    role: String,
+    content: String,
+}
+
+#[derive(Serialize)]
+struct ChatRequest<'a> {
+    model: &'a str,
+    messages: Vec<RequestMessage<'a>>,
+}
+
+#[derive(Serialize, Clone, Debug)]
+pub struct RequestMessage<'a> {
+    pub role: &'a str,
+    pub content: String,
+}
+
+pub async fn get_gpt_response(
+    messages: Vec<RequestMessage<'_>>,
+    gpt_token: &str,
+    client: &reqwest::Client,
+) -> Result<String, Error> {
+    const URL: &str = "https://api.openai.com/v1/chat/completions";
+    const GPT_MODEL: &str = "gpt-3.5-turbo";
+
+    let request_body = ChatRequest {
+        model: GPT_MODEL,
+        messages,
+    };
+
+    let response = client
+        .post(URL)
+        .header("Content-Type", "application/json")
+        .header("Authorization", format!("Bearer {}", gpt_token))
+        .json(&request_body)
+        .send()
+        .await?
+        .json::<ChatResponse>()
+        .await?;
+
+    log::info!("Response content: {}", response.choices[0].message.content);
+
+    return Ok(response.choices[0].message.content.clone());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,90 @@
-use anyhow::anyhow;
+mod chatgpt;
+
+use chatgpt::get_gpt_response;
+use chatgpt::RequestMessage;
+use regex::Regex;
+
+use anyhow::Context as _;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
+use serenity::model::user::User;
 use serenity::prelude::*;
+use serenity::utils::MessageBuilder;
 use shuttle_secrets::SecretStore;
 use tracing::{error, info};
-use regex::Regex;
 
-struct Bot;
+struct Bot {
+    gpt_token: String,
+    client: reqwest::Client,
+}
+
+fn is_user(author: &User) -> bool {
+    return !author.bot;
+}
+
+fn is_inclued_bot_mention(ctx: &Context, message: &Message) -> bool {
+    return message
+        .mentions
+        .iter()
+        .any(|user| user.id == ctx.cache.current_user_id());
+}
+
+fn build_json(messages: Vec<Message>) -> Vec<RequestMessage<'static>> {
+    let mention_regexp = Regex::new(r"<@(\d+)>").unwrap();
+    return messages
+        .iter()
+        .rev()
+        .map(|message| {
+            let content = mention_regexp.replace_all(&message.content, "").to_string();
+
+            let role = match is_user(&message.author) {
+                true => "user",
+                _ => "assistant",
+            };
+            RequestMessage { role, content }
+        })
+        .collect();
+}
 
 #[async_trait]
 impl EventHandler for Bot {
     async fn message(&self, ctx: Context, msg: Message) {
+        if is_inclued_bot_mention(&ctx, &msg) && is_user(&msg.author) {
+            let channel_id = msg.channel_id;
+            let messages = match channel_id.messages(&ctx.http, |m| m.limit(100)).await {
+                Ok(messages) => messages,
+                Err(e) => {
+                    println!("Error fetching messages: {}", e);
+                    return;
+                }
+            };
+
+            let requset_body: Vec<RequestMessage> = build_json(messages);
+            let _typing = match msg.channel_id.start_typing(&ctx.http) {
+                Ok(typing) => typing,
+                Err(e) => {
+                    println!("Error: {}", e);
+                    return;
+                }
+            };
+            let gpt_message =
+                match get_gpt_response(requset_body, &self.gpt_token, &self.client).await {
+                    Ok(text) => text,
+                    Err(e) => {
+                        println!("Error GPT response: {}", e);
+                        return;
+                    }
+                };
+            let response = MessageBuilder::new()
+                .mention(&msg.author)
+                .push(&gpt_message)
+                .build();
+            if let Err(why) = msg.channel_id.say(&ctx.http, &response).await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+
         if msg.content == "ぬるぽ" {
             if let Err(e) = msg.channel_id.say(&ctx.http, "ガッ").await {
                 error!("Error sending message: {:?}", e);
@@ -50,26 +123,39 @@ impl EventHandler for Bot {
     async fn ready(&self, _: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
     }
+
+    #[cfg(feature = "cache")]
+    async fn cache_ready(&self, _ctx: Context, _guilds: Vec<GuildId>) {
+        println!("cache ready");
+    }
 }
 
 #[shuttle_runtime::main]
 async fn serenity(
     #[shuttle_secrets::Secrets] secret_store: SecretStore,
 ) -> shuttle_serenity::ShuttleSerenity {
-    // Get the discord token set in `Secrets.toml`
-    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
-        token
-    } else {
-        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
-    };
+    let discord_token = secret_store
+        .get("DISCORD_TOKEN")
+        .context("'DISCORD_TOKEN' was not found")?;
+    let gpt_token = secret_store
+        .get("CHATGPT_TOKEN")
+        .context("'CHATGPT_TOKEN' was not found")?;
 
-    // Set gateway intents, which decides what events the bot will be notified about
-    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
-
-    let client = Client::builder(&token, intents)
-        .event_handler(Bot)
-        .await
-        .expect("Err creating client");
+    let client = get_client(&discord_token, &gpt_token).await;
 
     Ok(client.into())
+}
+
+pub async fn get_client(discord_token: &str, gpt_token: &str) -> serenity::Client {
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    serenity::Client::builder(discord_token, intents)
+        .event_handler(Bot {
+            gpt_token: gpt_token.to_owned(),
+            client: reqwest::Client::new(),
+        })
+        .await
+        .expect("Err creating client")
 }
