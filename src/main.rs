aa15728688d2b2b93eef2bfed878f78d22944003
luminus-xd <eleline.dev@gmail.com>
2024-01-25 21:02:43 +0900

    Xのfixup機能を削除

--- src/main.rs
+++ src/main.rs
@@ -106,28 +106,6 @@ impl EventHandler for Bot {
                 error!("Error sending message: {:?}", e);
             }
         }
-
-        if msg.content.contains("https://x.com") || msg.content.contains("https://twitter.com") {
-            let re = match Regex::new(r"(https://(x\.com|twitter\.com)[^\s]+)") {
-                Ok(re) => re,
-                Err(err) => {
-                    error!("Error creating regex: {:?}", err);
-                    return;
-                }
-            };
-            let mut replaced_urls = Vec::new();
-            for cap in re.captures_iter(&msg.content) {
-                replaced_urls.push(
-                    cap[0]
-                        .replace("twitter.com", "vxtwitter.com")
-                        .replace("x.com", "vxtwitter.com"),
-                );
-            }
-            let urls_string = replaced_urls.join("\n");
-            if let Err(e) = msg.channel_id.say(&ctx.http, urls_string).await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
     }
 
     async fn ready(&self, ctx: Context, ready: Ready) {
