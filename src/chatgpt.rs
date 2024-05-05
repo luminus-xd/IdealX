66b97f8405991df16e43e99415da770b292315cf
luminus-xd <eleline.dev@gmail.com>
2024-05-05 15:51:26 +0900

    Updata: GPT Model

--- src/chatgpt.rs
+++ src/chatgpt.rs
@@ -36,7 +36,7 @@ pub async fn get_gpt_response(
     client: &reqwest::Client,
 ) -> Result<String, Error> {
     const URL: &str = "https://api.openai.com/v1/chat/completions";
-    const GPT_MODEL: &str = "gpt-4-turbo";
+    const GPT_MODEL: &str = "gpt-4-turbo-2024-04-09";
 
     let request_body = ChatRequest {
         model: GPT_MODEL,
